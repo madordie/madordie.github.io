2e6da0657c5bc4b899a4e6ad5124b0d9413b37a2
madordie <e.madordie@gmail.com>
2020-10-13 11:40:09 +0800

    backup

--- themes/next/source/js/local-search.js
+++ themes/next/source/js/local-search.js
@@ -2,7 +2,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   if (!CONFIG.path) {
-    // Search DB path
     console.warn('`hexo-generator-searchdb` plugin is not installed!');
     return;
   }
@@ -197,8 +196,10 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   const fetchData = () => {
+    // Search DB path
+    const searchPath = CONFIG.root + CONFIG.path;
     const isXml = !CONFIG.path.endsWith('json');
-    fetch(CONFIG.path)
+    fetch(searchPath)
       .then(response => response.text())
       .then(res => {
         // Get the contents from search data
